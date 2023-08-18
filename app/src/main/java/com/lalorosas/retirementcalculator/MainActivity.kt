9fb1440e04c28fd7f1dc809abfa83b8966d29303
Kiran Shamsundar <kiran.shamsundar@gmail.com>
2023-08-18 13:50:20 +0530

    Initial commit

---
+++ app/src/main/java/com/lalorosas/retirementcalculator/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.lalorosas.retirementcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        AppCenter.start(application, "105a2095-ab88-4f57-a366-47068b73d899", Analytics::class.java, Crashes::class.java);
+
+        calculateButton.setOnClickListener {
+            // Crashes.generateTestCrash()
+            try {
+                val interestRate = interestEditText.text.toString().toFloat()
+                val currentAge = ageEditText.text.toString().toInt()
+                val retirementAge = retirementEditText.text.toString().toInt()
+                val monthly = monthlySavingsEditText.text.toString().toFloat()
+                val current = currentEditText.text.toString().toFloat()
+
+                val properties:HashMap<String, String> = HashMap<String, String>()
+                properties.put("interest_rate", interestRate.toString())
+                properties.put("current_age", currentAge.toString())
+                properties.put("retirement_age", retirementAge.toString())
+                properties.put("monthly_savings", monthly.toString())
+                properties.put("current_savings", current.toString())
+
+                if (interestRate <= 0) {
+                    Analytics.trackEvent("wrong_interest_rate", properties)
+                }
+                if (retirementAge <= currentAge) {
+                    Analytics.trackEvent("wrong_age", properties)
+                }
+
+                resultTextView.text = "At the current rate of $interestRate%, saving \$$monthly a month you will have \$X by $retirementAge."
+            } catch(ex: Exception){
+                Analytics.trackEvent(ex.message)
+            }
+        }
+    }
+}
